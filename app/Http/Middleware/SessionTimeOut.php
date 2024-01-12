dc97d766b583d65f08a894284aa9c22e31fb6d15
ausman95 <ausman.sakala95@gmail.com>
2024-01-12 09:45:44 +0200

    aa
    a

--- app/Http/Middleware/SessionTimeOut.php
+++ app/Http/Middleware/SessionTimeOut.php
@@ -1,51 +1,51 @@
 <?php
-
-namespace App\Http\Middleware;
-
-use Closure;
-use Illuminate\Http\Request;
-
-class SessionTimeOut
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
-
-    public function handle($request, Closure $next)
-    {
-        // session()->forget('lastActivityTime');
-
-        if (! session()->has('lastActivityTime')) {
-            session(['lastActivityTime' => now()]);
-        }
-
-        // dd(
-        //     session('lastActivityTime')->format('Y-M-jS h:i:s A'),
-        //     now()->diffInMinutes(session('lastActivityTime')),
-        //     now()->diffInMinutes(session('lastActivityTime')) >= config('session.lifetime')
-        // );
-
-        if (now()->diffInMinutes(session('lastActivityTime')) >= (2) ) {  // also you can this value in your config file and use here
-            if (auth()->check() && auth()->id() > 1) {
-                $user = auth()->user();
-                auth()->logout();
-
-                $user->update(['is_logged_in' => false]);
-                $this->reCacheAllUsersData();
-
-                session()->forget('lastActivityTime');
-
-                return redirect(route('users.login'));
-            }
-
-        }
-        session(['lastActivityTime' => now()]);
-
-        return $next($request);
-    }
-
-}
+//
+//namespace App\Http\Middleware;
+//
+//use Closure;
+//use Illuminate\Http\Request;
+//
+//class SessionTimeOut
+//{
+//    /**
+//     * Handle an incoming request.
+//     *
+//     * @param  \Illuminate\Http\Request  $request
+//     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+//     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+//     */
+//
+//    public function handle($request, Closure $next)
+//    {
+//        // session()->forget('lastActivityTime');
+//
+//        if (! session()->has('lastActivityTime')) {
+//            session(['lastActivityTime' => now()]);
+//        }
+//
+//        // dd(
+//        //     session('lastActivityTime')->format('Y-M-jS h:i:s A'),
+//        //     now()->diffInMinutes(session('lastActivityTime')),
+//        //     now()->diffInMinutes(session('lastActivityTime')) >= config('session.lifetime')
+//        // );
+//
+//        if (now()->diffInMinutes(session('lastActivityTime')) >= (2) ) {  // also you can this value in your config file and use here
+//            if (auth()->check() && auth()->id() > 1) {
+//                $user = auth()->user();
+//                auth()->logout();
+//
+//                $user->update(['is_logged_in' => false]);
+//                $this->reCacheAllUsersData();
+//
+//                session()->forget('lastActivityTime');
+//
+//                return redirect(route('users.login'));
+//            }
+//
+//        }
+//        session(['lastActivityTime' => now()]);
+//
+//        return $next($request);
+//    }
+//
+//}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="refresh" content="{{ config('session.lifetime') * 60 }}; url=/logout">
+    <meta http-equiv="refresh" content="{{ config('session.lifetime') * 60 }}; url={{route('login')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 {{--    <title>SICO CIVILS - Constructing the Future @yield('page')</title>--}}
         <link rel="stylesheet" href="{{asset('vendors/select2/css/select2.css')}}">
