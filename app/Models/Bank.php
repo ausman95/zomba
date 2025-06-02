44281a2789d5db4fc04585ec05be80ac6b7617a4
ausman95 <104424341+ausman95@users.noreply.github.com>
2025-06-02 14:43:34 +0200

    a

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -30,20 +30,20 @@ class Bank extends Model
 
     public function getBalance()
     {
-//        // Calculate the sum of amounts where type = 1
-//        $revenue = Payment::where('bank_id', $this->id)
-//            ->where('status', 1)
-//            ->where('type', 1)
-//            ->sum('amount');
-//
-//        // Calculate the sum of amounts where type = 2
-//        $expenses = Payment::where('bank_id', $this->id)
-//            ->where('type', 2)
-//            ->where('status', 1)
-//            ->sum('amount');
-//
-//        // Return the difference between revenue and expenses
-//        return $revenue - $expenses;
+        // Calculate the sum of amounts where type = 1
+        $revenue = Payment::where('bank_id', $this->id)
+            ->where('status', 0)
+            ->where('type', 1)
+            ->sum('amount');
+
+        // Calculate the sum of amounts where type = 2
+        $expenses = Payment::where('bank_id', $this->id)
+            ->where('type', 2)
+            ->where('status', 0)
+            ->sum('amount');
+
+        // Return the difference between revenue and expenses
+        return $revenue - $expenses;
     }
 
 }
