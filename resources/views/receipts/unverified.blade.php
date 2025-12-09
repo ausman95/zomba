8b58cd35f3c01b58c8acb21380b0629f3e344abb
ausman95 <104424341+ausman95@users.noreply.github.com>
2025-12-09 11:05:57 +0200

    Update unverified.blade.php

--- resources/views/receipts/unverified.blade.php
+++ resources/views/receipts/unverified.blade.php
@@ -151,15 +151,17 @@
 
                                             @foreach($transactions as $transaction)
                                                 @php
-                                                    $isRevenue = (($transaction->type ?? null) == 1);
-                                                    $displayAmount = $transaction->amount;
+                                                    $isRevenue = (
+                                                        ($transaction->type === 1) ||
+                                                        ($transaction->specification === 'Payroll Payment')
+                                                    );                                                    $displayAmount = $transaction->amount;
 
-                                                    if ($isRevenue) {
-                                                        $runningBalance += $displayAmount;
-                                                    } else {
-                                                        $runningBalance -= $displayAmount;
-                                                    }
-                                                    $counter++; // Increment for the current transaction
+                                                                                                        if ($isRevenue) {
+                                                                                                            $runningBalance += $displayAmount;
+                                                                                                        } else {
+                                                                                                            $runningBalance -= $displayAmount;
+                                                                                                        }
+                                                                                                        $counter++; // Increment for the current transaction
                                                 @endphp
                                                 <tr>
                                                     <td>{{ $counter }}</td>
@@ -168,7 +170,12 @@
                                                     <td>{{ $transaction->specification ?? 'N/A' }}</td>
                                                     <td class="text-end">
                                                         @if($isRevenue)
-                                                            {{ number_format($displayAmount, 2) }}
+                                                            @if($transaction->specification=='Payroll Payment')
+                                                                {{ number_format(abs($displayAmount), 2) }}
+                                                            @else
+                                                                {{ number_format($displayAmount, 2) }}
+                                                            @endif
+
                                                         @else
                                                             -
                                                         @endif
